a5995c35d67ff38d79eb2f2c16411f1512a1bfdd
Acura82 <gemxyj1982@sina.com>
2020-06-22 12:19:33 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+from python.demo1 import Demo1
+# from testing import demo2
+from testing.demo2 import Demo2
+from testing.demo2 import *
+# Demo1()
+# Demo2()
+# print(hello)
+# f()
+# print(demo2.hello)
+# demo2.f()
+
+from testing import *
+
+demo2
+demo3
